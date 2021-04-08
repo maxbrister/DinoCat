d877a701c1567286baec8fb70a77eeb1ea1e90aa
Max Brister <mabriste@microsoft.com>
2021-04-08 12:41:55 -0700

    Add baseline tests

---
+++ tests/DinoCat.Base.Tests/BaselineTests.cs
@@ -0,0 +1,104 @@
+using DinoCat.Base.Tests.Mocks;
+using DinoCat.Drawing;
+using DinoCat.Elements;
+using DinoCat.Tree;
+using System.Collections.Generic;
+using Xunit;
+using static DinoCat.Elements.Factories;
+
+namespace DinoCat.Base.Tests
+{
+    public class BaselineTests
+    {
+        Context context;
+
+        public BaselineTests()
+        {
+            StateManager stateManager = new(_ => { });
+            context = new(stateManager, new Layer(), new());
+        }
+
+        [Fact]
+        public void RowBaseline()
+        {
+            var element = Row(VerticalAlignment.Baseline,
+                new SetBaseline(Rectangle(Colors.Red, 10, 100), 0),
+                Rectangle(Colors.Blue, 10, 100));
+            var node = element.CreateNode(null, context);
+            var (size, baseline) = node.Arrange(new(500, 500));
+            Assert.Equal(new(20, 200), size);
+            Assert.Equal(100, baseline);
+        }
+
+        [Fact]
+        public void RowBaseline_SkipTop()
+        {
+            var element = Row(VerticalAlignment.Baseline,
+                new SetBaseline(Rectangle(Colors.Red, 10, 100), 0).Top(),
+                Rectangle(Colors.Blue, 10, 100));
+            var node = element.CreateNode(null, context);
+            var (size, baseline) = node.Arrange(new(500, 500));
+            Assert.Equal(new(20, 100), size);
+            Assert.Equal(100, baseline);
+        }
+
+        [Fact]
+        public void ColumnBaseline()
+        {
+            var element = Column(
+                Rectangle(Colors.Red, 10, 10),
+                new SetBaseline(Rectangle(Colors.Green, 10, 10), 5),
+                Rectangle(Colors.Blue, 10, 10));
+            var node = element.CreateNode(null, context);
+            var (size, baseline) = node.Arrange(new(500, 500));
+            Assert.Equal(new(10, 30), size);
+            Assert.Equal(15, baseline);
+        }
+
+        internal class SetBaseline : Element
+        {
+            public SetBaseline(Element child, float? baseline)
+            {
+                Child = child;
+                Baseline = baseline;
+            }
+
+            public Element Child { get; }
+            public float? Baseline { get; }
+
+            public override Node CreateNode(Node? parent, Context context) =>
+                new SetBaselineNode(parent, context, this);
+        }
+
+        internal class SetBaselineNode : NodeBase<SetBaseline>
+        {
+            Node child;
+
+            public SetBaselineNode(Node? parent, Context context, SetBaseline element) : base(parent, context, element)
+            {
+                child = element.Child.CreateNode(this, context);
+            }
+
+            public override IEnumerable<Node> Children
+            {
+                get
+                {
+                    yield return child;
+                }
+            }
+
+            protected override (Size, float?) ArrangeOverride(Size availableSize)
+            {
+                var (size, _) = child.Arrange(availableSize);
+                return (size, Element.Baseline);
+            }
+
+            protected override void UpdateElement(SetBaseline oldElement, Context oldContext)
+            {
+                if (oldElement.Baseline != Element.Baseline)
+                    Context.InvalidateLayout();
+                child = child.UpdateElement(Element.Child, Context);
+            }
+        }
+    }
+}
---
+++ tests/DinoCat.Base.Tests/Mocks/Layer.cs
@@ -0,0 +1,51 @@
+﻿using DinoCat.Drawing;
+using DinoCat.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinoCat.Base.Tests.Mocks
+{
+    class Layer : ILayer
+    {
+        List<Layer> children = new();
+        ILayerNode? node;
+
+        public Layer(ILayerNode? node = null)
+        {
+            this.node = node;
+        }
+
+        public ILayer AddChild(ILayerNode child)
+        {
+            Layer layer = new(child);
+            children.Add(layer);
+            return layer;
+        }
+
+        public void Dispose() { }
+
+        public void Focus() { }
+
+        public void FocusNext() { }
+
+        public void FocusPrevious() { }
+
+        public void InvalidateLayout() { }
+
+        public void InvalidateRender() { }
+
+        public void OnArrange((Size, float?) size) { }
+
+        public void OnRender(DrawingContext drawingContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUpdated() { }
+
+        public void Reparent(ILayer newParent) { }
+    }
+}
